a125ff63a14a7f71a79bccf7ec6964f7c23fc28e
jocelynchin <42010403+jocelynchin@users.noreply.github.com>
2018-08-01 12:58:12 -0700

    Add files via upload

---
+++ website.js
@@ -0,0 +1,10 @@
+//navigation bar
+<div class=tab>
+<button type="button" onclick="document.getElementById('home').innerHTML=Home">Home</button>
+<button type="button" onclick="document.getElementById('mission').innerHTML=Mission">Mission</button>
+<button type="button" onclick="document.getElementById('whoarewe').innerHTML=Who_Are_We">Who Are We</button>
+<button type="button" onclick="document.getElementById('projects').innerHTML=Projects">Projects</button>
+<button type="button" onclick="document.getElementById('joinus').innerHTML=JoinUs">Join Us</button>
+</div>
+
+<button type="button" onclick="document.getElementById('Join')">Home</button>
